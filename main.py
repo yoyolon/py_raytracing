9fd9a3b47f57b974c28150dc10efdc900fd93a99
yoyolon <yoyograph74@gmail.com>
2022-12-09 22:15:42 +0900

    [fix] コードを短く

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
 import numpy as np
 from PIL import Image
-import time
 
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-WINDOW_RATIO = WINDOW_WIDTH / WINDOW_HEIGHT
-INFTY = float('inf')
-EPSILON = 0.0001
-ZERO = np.zeros(3)
-FOCAL_LENGTH = 1.0
-INV_GAMMA = 1 / 1.0
-BG_COLOR = np.array((1.0, 1.0, 1.0))
-DIFFURE = 1
-SPECULAR = 2
+WINDOW_WIDTH = 640; WINDOW_HEIGHT = 480; WINDOW_RATIO = WINDOW_WIDTH / WINDOW_HEIGHT
+INFTY = float('inf'); EPSILON = 0.0001
+RED = np.array((1.0,0.2,0.2)); GREEN = np.array((0.2,1.0,0.2)); BLUE = np.array((0.2,0.2,1.0))
+BLACK = np.array((0.0,0.0,0.0)); WHITE = np.array((1.0,1.0,1.0))
+
+def normalize(v): # 単位ベクトルを計算
+    return v / np.linalg.norm(v)
+
+def reflect(v, n): # 正反射方向を計算
+    r = -v * np.dot(v, n) + 2 * n
+    return normalize(r)
 
 class Ray:
     def __init__(self, o, d):
-        self.orig = o
-        self.dir = d
-
+        self.orig = o; self.dir = d
     def pos(self, t):
         return self.orig + t * self.dir
 
-
 class Intersection:
-    def __init__(self, p=None, t=None, m=None, n=None):
-        self.pos = p
-        self.t = t
-        self.mat = m
-        self.normal = n
+    def __init__(self, p=None, n=None, m=None):
+        self.p = p; self.n = n; self.mat = m
 
+class Material:
+    def __init__(self, d, s, a=5):
+        self.kd = d; self.ks = s; self.alpha =a    
+    def shading(self, isect, v_dir, l_pos):
+        v = normalize(v_dir); l = normalize(l_pos - isect.p)
+        weight = np.dot(reflect(v, isect.n), l) ** self.alpha
+        return self.kd * np.dot(isect.n, l) + self.ks * weight
 
 class Sphere:
     def __init__(self, r, c, m):
-        self.radius = r
-        self.center = c
-        self.mat = m
-    
+        self.r = r; self.c = c; self.mat = m
     def intersect(self, r, isect, t_max=INFTY, t_min=EPSILON):
-        temp = r.orig - self.center
+        temp = r.orig - self.c
         a = np.dot(r.dir, r.dir)
         b_half = np.dot(r.dir, temp)
-        c = np.dot(temp, temp) - self.radius**2
+        c = np.dot(temp, temp) - self.r**2
         D = b_half**2 - a*c
         if D < 0: return False
         b = b_half * 2
@@ -51,39 +48,53 @@ class Sphere:
             t = (-b + d) / (2 * a)
             if t < t_min or t > t_max:
                 return False
-        isect.t = t
-        isect.pos = r.pos(isect.t)
-        n = isect.pos - self.center
-        isect.normal = n / np.linalg.norm(n)
+        isect.p = r.pos(t)
+        isect.n = normalize(isect.p - self.c)
         isect.mat = self.mat
         return True
 
-scene = []
-sphere = Sphere(2.0, np.array((0.0,0.0,-5.0)), DIFFURE)
-scene.append(sphere)
+class PointLight:
+    def __init__(self, p, r):
+        self.p = p; self.L = r
+    def emit(self, obj_pos):
+        d = obj_pos - self.p
+        return self.L / np.dot(d, d)
+    def is_visible(self, isect, scene):
+        sr = Ray(isect.p, self.p - isect.p)
+        for obj in scene.objects:
+            if obj.intersect(sr, Intersection()):
+                return False
+        return True
 
-def raytracing(r):
-    for obj in scene:
+class Scene:
+    def __init__(self):
+        self.objects = []; self.lights = []
+        mr = Material(RED,WHITE,40); mg = Material(GREEN,WHITE,20); mb = Material(BLUE,WHITE,10)
+        s1 = Sphere(2.0, np.array((0.0,np.sqrt(3.0),-10.0)), mr)
+        s2 = Sphere(2.0, np.array((2.0,-np.sqrt(3.0),-10.0)), mg)
+        s3 = Sphere(2.0, np.array((-2.0,-np.sqrt(3.0),-10.0)), mb)
+        self.objects.extend([s1,s2,s3])
+        self.lights.append(PointLight(np.array((-3.0,4.0,-2.0)), np.array((25.0,25.0,25.0))))
+
+def raytracing(r, scene):
+    color = BLACK
+    for obj in scene.objects:
         isect = Intersection()
         if obj.intersect(r, isect):
-            return isect.normal * 0.5 + 0.5
-    return BG_COLOR
+            for light in scene.lights:
+                if light.is_visible(isect, scene):
+                    color = color + light.emit(isect.p) * obj.mat.shading(isect, r.dir, light.p)
+            return color
+    return WHITE
 
 if __name__=="__main__":
-    t_start = time.time()
-    array = np.zeros((WINDOW_HEIGHT, WINDOW_WIDTH, 3))
+    scene = Scene()
+    array = np.zeros((WINDOW_HEIGHT,WINDOW_WIDTH,3))
     for i in range(WINDOW_HEIGHT):
         for j in range(WINDOW_WIDTH):
-            u = j / WINDOW_WIDTH * -2 + 1.0
-            v = i / WINDOW_HEIGHT * -2 + 1.0
-            w = -FOCAL_LENGTH
-            dir = np.array((u*WINDOW_RATIO, v, w))
-            r = Ray(ZERO, dir/np.linalg.norm(dir))
-            array[i][j] = raytracing(r)
-    # 画像を保存
-    array = 255 * (array ** INV_GAMMA) # ガンマ補正
-    array = array.astype(np.uint8)    
-    img = Image.fromarray(array)
+            u = j/WINDOW_WIDTH*-2+1.0; v = i/WINDOW_HEIGHT*-2+1.0
+            dir = np.array((u*WINDOW_RATIO,v,-2.0))
+            array[i][j] = raytracing(Ray(np.zeros(3),normalize(dir)),scene)
+    array = 255 * (np.clip(array,0.0,1.0) ** (1/2.2))
+    img = Image.fromarray(array.astype(np.uint8))
     img.save("img.png")
-    t_end = time.time()
-    print("time: ", t_end-t_start)
